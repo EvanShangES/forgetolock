42bdf8eccd489fdb25c740f3b37aa162901d14c2
honeydewd <honeydewdes@gmail.com>
2019-09-26 22:33:57 -0400

    Testing for phone GPS

--- backend/config/databases.js
+++ backend/config/databases.js
@@ -1,7 +1,7 @@
 module.exports = {
     //For Local Usage
-    'url': 'mongodb://localhost/forgoLockLocal',
+    // 'url': 'mongodb://localhost/forgoLockLocal',
 
     //For Hosted Usage
-    // 'url': 'mongodb+srv://evanshang:123asdfgh@forgetalock-beta-zgrc1.mongodb.net/test?retryWrites=true&w=majority'
+    'url': 'mongodb+srv://evanshang:123asdfgh@forgetalock-beta-zgrc1.mongodb.net/test?retryWrites=true&w=majority'
 };

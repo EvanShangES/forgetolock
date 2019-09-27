e23cdbaa215da3b9b5f70a69bc7f5a7e2eff3f4d
honeydewd <honeydewdes@gmail.com>
2019-09-26 22:23:02 -0400

    Testing for phone GPS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './components/general/Home';
+import Register from './components/general/Register';
+import Login from './components/general/Login';
+import Dashboard from './components/general/Dashboard';
+import ServiceAreas from "./components/general/ServiceAreas";
+import NavMenu from "./components/layout/NavMenu";
+
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+import {setCurrentUser, loggedIn, logout} from "./actions/authActions";
+
+
+//check if user is logged in.
+loggedIn(function(response){
+    if(response.status === 200){
+        store.dispatch(setCurrentUser(response));
+    }else{
+        logout();
+    }
+});
+
+
+class App extends Component {
+    render(){
+        return (
+            <Provider store={store}>
+                <Router>
+                    <NavMenu/>
+                    <Route exact path="/" render={() => <Home/>}/>
+                    <Route exact path="/home" render={() => <Home/>}/>
+                    <Route exact path="/register" render={() => <Register/>}/>
+                    <Route exact path="/login" render={() => <Login/>}/>
+                    <Route exact path="/dashboard" render={() => <Dashboard/>}/>
+                    <Route exact path="/serviceAreas" render={() => <ServiceAreas/>}/>
+                </Router>
+            </Provider>
+        )
+    }
 }
 
 export default App;
